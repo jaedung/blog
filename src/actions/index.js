c25bc86c0d783642dd35b77bfbb2597a3da74278
justin <justinkim0310@gmail.com>
2019-12-11 08:36:42 +1000

    action creator in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,18 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(userId => dispatch(fetchUser(userId)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(userId => dispatch(fetchUser(userId)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -11,3 +25,15 @@ export const fetchUser = id => async dispatch => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// export const fetchUser = function(id) {
+//   return function(dispatch) {
+//     return _fetchUser(id, dispatch);
+//   };
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
